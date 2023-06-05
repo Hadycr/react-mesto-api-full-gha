ef1939fc5fb95ca10062741f303dee6292c41bda
Nadya <hadycr@gmail.com>
2023-06-05 23:56:42 +0400

    Добавлен winston

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 const router = require('./routes/index');
 const defaultError = require('./errors/defaultError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -28,9 +29,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(bodyParser.json());
-
+app.use(requestLogger);
 app.use('/', router);
-
+app.use(errorLogger);
 app.use(errors());
 app.use(defaultError);
 
