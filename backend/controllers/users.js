3ae19e52121b9c51b41c6b6e1b23a834ab86412b
Nadya <hadycr@gmail.com>
2023-06-06 22:46:22 +0400

    Добалвен env и краш

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -88,9 +88,10 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  const { NODE_ENV, JWT_SECRET } = process.env;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.send({ token, message: 'Всё верно!' });
     })
     .catch(next);
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,13 +3,14 @@ const NotAuthorizationError = require('../errors/notAuthError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  const { NODE_ENV, JWT_SECRET } = process.env;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new NotAuthorizationError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     return next(new NotAuthorizationError('Необходима авторизация'));
   }
